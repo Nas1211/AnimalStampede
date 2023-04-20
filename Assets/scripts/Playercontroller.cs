629346f689a72cf2cf8fcf24f04242bfecc2c1b4
NAS <boygiant8@gmail.com>
2023-04-20 14:05:21 -0400

    move player

--- Assets/scripts/Playercontroller.cs
+++ Assets/scripts/Playercontroller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Playercontroller : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,11 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis(“Horizontal”);
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10,transform.position.y,transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
